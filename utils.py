f67ebf4f8d5217c9d6beefdabf63329d42e5e1e5
HyeonJaeGil <now9728@gmail.com>
2022-07-21 21:44:41 +0900

    finished interpolated gt construction codes

--- utils.py
+++ utils.py
@@ -38,33 +38,16 @@ def read_interpolated_pose(file_path):
     return np.asarray(rows, dtype='float64')
 
 
-def show_loop_images(gt_pose_table, img_folder, id1, id2):
+# read csv file that contains local pose
+def read_pairs(file_path): 
 
-    print(id1, id2)
-    img1_filename = gt_pose_table[id1-1, 1]
-    img2_filename = gt_pose_table[id2-1, 1]
-    img1 = np.array(cv2.imread(os.path.join(img_folder,img1_filename)))
-    img2 = np.array(cv2.imread(os.path.join(img_folder,img2_filename)))
-
-    # only for visualization
-    imgNormalize1 = cv2.normalize(img1,None,0,255,cv2.NORM_MINMAX)
-    imgNormalize2 = cv2.normalize(img2,None,0,255,cv2.NORM_MINMAX)
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    org = (30, 30)
-    fontScale = 1
-    color = (255, 0, 0)
-    thickness = 2
-
-    img1 = cv2.putText(imgNormalize1, 'id=' + str(id1), org, font, 
-                    fontScale, color, thickness, cv2.LINE_AA)
-    img2 = cv2.putText(imgNormalize2, 'id=' + str(id2), org, font, 
-                    fontScale, color, thickness, cv2.LINE_AA)
-    img_pair = np.concatenate((img1, img2), axis=1)
-
-    cv2.imshow('loop', img_pair)
-    cv2.waitKey(0) 
-
+    with open(file_path, 'r') as file:
+        csvreader = csv.reader(file)
+        rows = []
+        for row in csvreader:
+            rows.append(row)
+    
+    return np.asarray(rows, dtype='float64')
 
 
 def read_files_from_path(file_path):
@@ -73,6 +56,30 @@ def read_files_from_path(file_path):
         data = np.array(cv2.imread(os.path.join(file_path,file)))
         data = os.path.join(file_path,file)
         print(data)
+
+
+def read_image(img_path):
+    if not os.path.isfile(img_path):
+        print(img_path, " [invalid image]")
+        return np.array([])
+    img = cv2.imread(img_path, -1).astype('float32')
+    min, max = np.amin(img), np.amax(img)
+    # min, max = 2500, 4000
+    img = (img - min) / (max - min)
+    np.clip(img, 0, 1, out=img)
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
+    img = np.expand_dims(img, axis=-1)
+    return img
+
+
+def read_image_16bit(img_path):
+    img = cv2.imread(img_path, -1).astype('float32')
+    min, max = np.amin(img), np.amax(img)
+    # min, max = 2500, 4000
+    img = (img - min) / (max - min)
+    np.clip(img, 0, 1, out=img)
+    img = np.expand_dims(img, axis=-1)
+    return img
 
 
 def find_nearest_leftmost(array, value):
@@ -119,13 +126,15 @@ def construct_gt_pose_array(image_folder, pose_table, output_path):
                 d2 = pose_table[idx+1, 0] - img_time
                 assert (d1 >= 0 and d2 >= 0)
                 
-                new_pose = pose_table[idx, 1:-1] * d2 / (d1+d2) + pose_table[idx+1, 1:-1] * d1 / (d1+d2)
+                # position handling
+                new_pose = pose_table[idx, 1:-3] * d2 / (d1+d2) + pose_table[idx+1, 1:-3] * d1 / (d1+d2)
 
-                if pose_table[idx, -1] < 0: 
+                # yaw angle handling
+                if pose_table[idx, -1] * pose_table[idx+1, -1] < 0 and abs(pose_table[idx, -1] - pose_table[idx+1, -1]) > np.float64(300) : 
                     new_yaw_1 = pose_table[idx, -1] + np.float64(360.0)
                 else: 
                     new_yaw_1 = pose_table[idx, -1]
-                if pose_table[idx+1, -1] < 0: 
+                if pose_table[idx+1, -1] * pose_table[idx+1, -1] < 0 and abs(pose_table[idx, -1] - pose_table[idx+1, -1]) > np.float64(300) : 
                     new_yaw_2 = pose_table[idx+1, -1] + np.float64(360.0)
                 else: 
                     new_yaw_2 = pose_table[idx+1, -1]
@@ -134,52 +143,52 @@ def construct_gt_pose_array(image_folder, pose_table, output_path):
                 if interpolated_yaw > np.float64(180.0):
                     interpolated_yaw -= np.float64(360.0)
 
-                writer.writerow( np.concatenate( ([count], [img], new_pose, [interpolated_yaw]), axis=0))
-            # writer.writerow( np.concatenate( ([img[:-4]], new_pose), axis=0))
+                # pitch angle handling
+                if pose_table[idx, -2] * pose_table[idx+1, -2] < 0 and abs(pose_table[idx, -2] - pose_table[idx+1, -2]) > np.float64(300) : 
+                    new_pitch_1 = pose_table[idx, -2] + np.float64(360.0)
+                else: 
+                    new_pitch_1 = pose_table[idx, -2]
+                if pose_table[idx+1, -2] * pose_table[idx+1, -2] < 0 and abs(pose_table[idx, -2] - pose_table[idx+1, -2]) > np.float64(300) : 
+                    new_pitch_2 = pose_table[idx+1, -2] + np.float64(360.0)
+                else: 
+                    new_pitch_2 = pose_table[idx+1, -2]
+                interpolated_pitch = new_pitch_1  * d2 / (d1+d2) + new_pitch_2 * d1 / (d1+d2)
+                
+                if interpolated_pitch > np.float64(180.0):
+                    interpolated_pitch -= np.float64(360.0)
+
+                # roll angle handling
+                if pose_table[idx, -3] * pose_table[idx+1, -3] < 0 and abs(pose_table[idx, -3] - pose_table[idx+1, -3]) > np.float64(300) : 
+                    new_roll_1 = pose_table[idx, -3] + np.float64(360.0)
+                else: 
+                    new_roll_1 = pose_table[idx, -3]
+                if pose_table[idx+1, -3] * pose_table[idx+1, -3] < 0 and abs(pose_table[idx, -3] - pose_table[idx+1, -3]) > np.float64(300) : 
+                    new_roll_2 = pose_table[idx+1, -3] + np.float64(360.0)
+                else: 
+                    new_roll_2 = pose_table[idx+1, -3]
+                interpolated_roll = new_roll_1  * d2 / (d1+d2) + new_roll_2 * d1 / (d1+d2)
+                
+                if interpolated_roll > np.float64(180.0):
+                    interpolated_roll -= np.float64(360.0)
+
+                writer.writerow( np.concatenate( ([count], [img], new_pose, 
+                            [interpolated_roll, interpolated_pitch, interpolated_yaw]), axis=0))
 
             count += 1
             # if count > 300:
             #     break
     print(count, "images were processed.")
 
-def visualize_gt_pose(gt_pose_file):
-    
-    count = 0
-    plt.clf()
-    x_list = []
-    y_list = []
-    with open(gt_pose_file, 'r') as f:
-        csvreader = csv.reader(f)
-        for row in csvreader:
-            count += 1
-            if count % 100 == 0:
-                x, y, z = row[2:5]
-                x_list.append(x)
-                y_list.append(y)
-            # print(x, y, z)
-    np_x_list = np.array(x_list)    
-    np_y_list = np.array(y_list)   
-    print(np_x_list) 
-    plt.plot(np_x_list, np_y_list)
-    plt.show()
-
-    # plt.xlim([-6000,600])
-    # plt.ylim([-1500,1500])
-    plt.legend()
-    plt.ion()
-
-
 
 if __name__ == '__main__':
     
     config_filename = './config/config.yaml'
     config = yaml.safe_load(open(config_filename))
-    src_root_path = config["src_root_path"]
+    src_root_path = config["src_root_path"]    
     dst_root_path = config["dst_root_path"]
     seq = config["seq"]
     src_root_path = os.path.join(src_root_path, seq)
     dst_root_path = os.path.join(dst_root_path, seq)
-
     image_folder = config["image_folder"]
     pose_folder = config["pose_folder"]
     output_local_gt_path = config["output_local_gt_path"]
@@ -190,10 +199,10 @@ if __name__ == '__main__':
     abs_output_local_gt_path = os.path.join(dst_root_path, output_local_gt_path)
     abs_output_global_gt_path = os.path.join(dst_root_path, output_global_gt_path)
 
-    local_pose_table = read_pose(os.path.join(abs_pose_folder, 'local_pose.csv'))
+    # local_pose_table = read_pose(os.path.join(abs_pose_folder, 'local_pose.csv'))
     global_pose_table = read_pose(os.path.join(abs_pose_folder, 'global_pose.csv'))
-    construct_gt_pose_array(abs_image_folder, local_pose_table, abs_output_local_gt_path)
-    construct_gt_pose_array(abs_image_folder, global_pose_table, abs_output_global_gt_path)
+    # construct_gt_pose_array(abs_image_folder, local_pose_table, abs_output_local_gt_path)
+    # construct_gt_pose_array(abs_image_folder, global_pose_table, abs_output_global_gt_path)
 
     # query = np.float64([-1.4168655872345, -0.863484025001526, 0.398936808109283])
     # print(query)
@@ -208,3 +217,13 @@ if __name__ == '__main__':
     # with open(output_gt_path, 'r') as file:
     #     csvreader = csv.reader(file)
     #     show_loop_images(f, )
+
+    img = read_image('/media/hj/seagate/datasets/sthereo_datasets/STHEREO/dataset_full/01/thermal_left/000149.png')
+    print(img, img.shape, img.dtype, np.amax(img), np.amin(img))
+    cv2.imshow('test', img.astype('uint8'))
+    cv2.waitKey(0)
+
+    # img = read_image('/media/hj/seagate/datasets/STHEREO/01/image/stereo_thermal_14_left/1630106835318041837.png')
+    # print(img, img.shape, img.dtype, np.amax(img), np.amin(img))
+    # cv2.imshow('test', img.astype('uint8'))
+    # cv2.waitKey(0)
